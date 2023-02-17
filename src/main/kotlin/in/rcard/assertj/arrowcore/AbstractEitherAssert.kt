ef0aff96064bcaf7dbc5f130ba674852453406c1
rcardin <riccardo.cardin@lastminute.com>
2023-02-17 15:26:52 +0100

    Added KDoc

--- src/main/kotlin/in/rcard/assertj/arrowcore/AbstractEitherAssert.kt
+++ src/main/kotlin/in/rcard/assertj/arrowcore/AbstractEitherAssert.kt
@@ -3,8 +3,22 @@ package `in`.rcard.assertj.arrowcore
 import arrow.core.Either
 import org.assertj.core.api.AbstractObjectAssert
 
+/**
+ * Assertions for [Either].
+ *
+ * @param <SELF>  the "self" type of this assertion class.
+ * @param <LEFT>  type of the left value contained in the [Either].
+ * @param <RIGHT> type of the right value contained in the [Either].
+ * @author Riccardo Cardin
+ */
 abstract class AbstractEitherAssert<SELF : AbstractEitherAssert<SELF, LEFT, RIGHT>, LEFT, RIGHT>(either: Either<LEFT, RIGHT>?) :
     AbstractObjectAssert<SELF, Either<LEFT, RIGHT>>(either, AbstractEitherAssert::class.java) {
+
+    /**
+     * Verifies that the actual [Either] is right.
+     *
+     * @return this assertion object.
+     */
     fun isRight() {
         isNotNull
         if (!actual.isRight()) {
--- src/main/kotlin/in/rcard/assertj/arrowcore/EitherAssert.kt
+++ src/main/kotlin/in/rcard/assertj/arrowcore/EitherAssert.kt
@@ -2,6 +2,13 @@ package `in`.rcard.assertj.arrowcore
 
 import arrow.core.Either
 
+/**
+ * Assertions for [Either].
+ *
+ * @param <LEFT>  type of the value on the left contained in the [Either].
+ * @param <RIGHT> type of the value on the right contained in the [Either].
+ * @author Riccardo Cardin
+ */
 class EitherAssert<LEFT, RIGHT>(either: Either<LEFT, RIGHT>?) :
     AbstractEitherAssert<EitherAssert<LEFT, RIGHT>, LEFT, RIGHT>(either) {
     companion object {
