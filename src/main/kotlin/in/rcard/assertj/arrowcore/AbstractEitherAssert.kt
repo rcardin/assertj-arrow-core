3be19b93e1cf50fd57ea6e4fd0afe06390fe4d1a
rcardin <riccardo.cardin@gmail.com>
2023-02-27 08:45:35 +0100

    Formatted file using ktlint

--- src/main/kotlin/in/rcard/assertj/arrowcore/AbstractEitherAssert.kt
+++ src/main/kotlin/in/rcard/assertj/arrowcore/AbstractEitherAssert.kt
@@ -19,93 +19,93 @@ import org.assertj.core.internal.StandardComparisonStrategy
  * @author Riccardo Cardin
  */
 abstract class AbstractEitherAssert<
-    SELF : AbstractEitherAssert<SELF, LEFT, RIGHT>, LEFT : Any, RIGHT : Any>(
-    either: Either<LEFT, RIGHT>?
+    SELF : AbstractEitherAssert<SELF, LEFT, RIGHT>, LEFT : Any, RIGHT : Any,>(
+    either: Either<LEFT, RIGHT>?,
 ) : AbstractObjectAssert<SELF, Either<LEFT, RIGHT>>(either, AbstractEitherAssert::class.java) {
 
-  private val comparisonStrategy: ComparisonStrategy = StandardComparisonStrategy.instance()
+    private val comparisonStrategy: ComparisonStrategy = StandardComparisonStrategy.instance()
 
-  /**
-   * Verifies that the actual [Either] is right.
-   *
-   * @return this assertion object.
-   */
-  fun isRight(): SELF {
-    isNotNull
-    assertIsRight()
-    return myself
-  }
-
-  /**
-   * Verifies that the actual [Either] is left.
-   *
-   * @return this assertion object.
-   */
-  fun isLeft(): SELF {
-    assertIsLeft()
-    return myself
-  }
-
-  /**
-   * Verifies that the actual [Either] is [Either.Right] and contains the given value.
-   *
-   * @param expectedValue the expected value inside the [Either].
-   * @return this assertion object.
-   */
-  fun containsOnRight(expectedValue: RIGHT): SELF {
-    assertIsRight()
-    actual.onRight { right ->
-      if (!comparisonStrategy.areEqual(right, expectedValue)) {
-        throwAssertionError(shouldContainOnRight(actual, expectedValue))
-      }
+    /**
+     * Verifies that the actual [Either] is right.
+     *
+     * @return this assertion object.
+     */
+    fun isRight(): SELF {
+        isNotNull
+        assertIsRight()
+        return myself
     }
-    return myself
-  }
 
-  /**
-   * Verifies that the actual right-sided [Either] contains a value that is an
-   * instance of the argument.
-   *
-   * @param expectedClass the expected class of the value inside the right-sided [Either].
-   * @return this assertion object.
-   */
-  fun containsRightInstanceOf(expectedClass: Class<*>): SELF {
-    assertIsRight()
-    actual.onRight { right ->
-      if (!expectedClass.isInstance(right)) {
-        throwAssertionError(shouldContainOnRightInstanceOf(actual, expectedClass))
-      }
+    /**
+     * Verifies that the actual [Either] is left.
+     *
+     * @return this assertion object.
+     */
+    fun isLeft(): SELF {
+        assertIsLeft()
+        return myself
     }
-    return myself
-  }
 
-  private fun assertIsRight() {
-    isNotNull
-    if (!actual.isRight()) {
-      throwAssertionError(shouldBeRight(actual))
+    /**
+     * Verifies that the actual [Either] is [Either.Right] and contains the given value.
+     *
+     * @param expectedValue the expected value inside the [Either].
+     * @return this assertion object.
+     */
+    fun containsOnRight(expectedValue: RIGHT): SELF {
+        assertIsRight()
+        actual.onRight { right ->
+            if (!comparisonStrategy.areEqual(right, expectedValue)) {
+                throwAssertionError(shouldContainOnRight(actual, expectedValue))
+            }
+        }
+        return myself
     }
-  }
 
-  /**
-   * Verifies that the actual [Either] is [Either.Left] and contains the given value.
-   *
-   * @param expectedValue the expected value inside the [Either].
-   * @return this assertion object.
-   */
-  fun containsOnLeft(expectedValue: LEFT): SELF {
-    assertIsLeft()
-    actual.onLeft { left ->
-      if (!comparisonStrategy.areEqual(left, expectedValue)) {
-        throwAssertionError(shouldContainOnLeft(actual, expectedValue))
-      }
+    /**
+     * Verifies that the actual right-sided [Either] contains a value that is an
+     * instance of the argument.
+     *
+     * @param expectedClass the expected class of the value inside the right-sided [Either].
+     * @return this assertion object.
+     */
+    fun containsRightInstanceOf(expectedClass: Class<*>): SELF {
+        assertIsRight()
+        actual.onRight { right ->
+            if (!expectedClass.isInstance(right)) {
+                throwAssertionError(shouldContainOnRightInstanceOf(actual, expectedClass))
+            }
+        }
+        return myself
     }
-    return myself
-  }
 
-  private fun assertIsLeft() {
-    isNotNull
-    if (!actual.isLeft()) {
-      throwAssertionError(shouldBeLeft(actual))
+    private fun assertIsRight() {
+        isNotNull
+        if (!actual.isRight()) {
+            throwAssertionError(shouldBeRight(actual))
+        }
     }
-  }
+
+    /**
+     * Verifies that the actual [Either] is [Either.Left] and contains the given value.
+     *
+     * @param expectedValue the expected value inside the [Either].
+     * @return this assertion object.
+     */
+    fun containsOnLeft(expectedValue: LEFT): SELF {
+        assertIsLeft()
+        actual.onLeft { left ->
+            if (!comparisonStrategy.areEqual(left, expectedValue)) {
+                throwAssertionError(shouldContainOnLeft(actual, expectedValue))
+            }
+        }
+        return myself
+    }
+
+    private fun assertIsLeft() {
+        isNotNull
+        if (!actual.isLeft()) {
+            throwAssertionError(shouldBeLeft(actual))
+        }
+    }
 }
--- src/test/kotlin/in/rcard/assertj/arrowcore/EitherAssert_containsOnRightInstanceOf_Test.kt
+++ src/test/kotlin/in/rcard/assertj/arrowcore/EitherAssert_containsOnRightInstanceOf_Test.kt
@@ -12,46 +12,53 @@ import org.junit.jupiter.api.Test
 
 class EitherAssert_containsOnRightInstanceOf_Test {
 
-  @Test
-  fun `should fail if either is null`() {
-    val actual: Either<Nothing, Nothing>? = null
-    Assertions.assertThatThrownBy { assertThat(actual).containsRightInstanceOf(Any::class.java) }
-        .isInstanceOf(AssertionError::class.java)
-        .hasMessage(FailureMessages.actualIsNull())
-  }
+    @Test
+    fun `should fail if either is null`() {
+        val actual: Either<Nothing, Nothing>? = null
+        Assertions.assertThatThrownBy {
+            assertThat(actual).containsRightInstanceOf(Any::class.java)
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(FailureMessages.actualIsNull())
+    }
 
-  @Test
-  fun `should fail if either is left`() {
-    val actual: Either<String, Nothing> = "some".left()
-    Assertions.assertThatThrownBy { assertThat(actual).containsRightInstanceOf(Any::class.java) }
-        .isInstanceOf(AssertionError::class.java)
-        .hasMessage(shouldBeRight(actual).create())
-  }
+    @Test
+    fun `should fail if either is left`() {
+        val actual: Either<String, Nothing> = "some".left()
+        Assertions.assertThatThrownBy {
+            assertThat(actual).containsRightInstanceOf(Any::class.java)
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(shouldBeRight(actual).create())
+    }
 
-  @Test
-  fun should_fail_if_either_contains_other_type_on_right_than_required() {
-    val actual: Either<Any, Int> = 42.right()
-    Assertions.assertThatThrownBy { assertThat(actual).containsRightInstanceOf(String::class.java) }
-        .isInstanceOf(AssertionError::class.java)
-        .hasMessage(
-            shouldContainOnRightInstanceOf(
+    @Test
+    fun should_fail_if_either_contains_other_type_on_right_than_required() {
+        val actual: Either<Any, Int> = 42.right()
+        Assertions.assertThatThrownBy {
+            assertThat(actual).containsRightInstanceOf(String::class.java)
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(
+                shouldContainOnRightInstanceOf(
                     actual,
                     String::class.java,
                 )
-                .create())
-  }
+                    .create(),
+            )
+    }
 
-  @Test
-  fun `should pass if either contains required type subclass on right`() {
-    val actual = Child().right()
-    assertThat(actual).containsRightInstanceOf(Parent::class.java)
-  }
+    @Test
+    fun `should pass if either contains required type subclass on right`() {
+        val actual = Child().right()
+        assertThat(actual).containsRightInstanceOf(Parent::class.java)
+    }
 
-  @Test
-  fun `should pass if either contains required type on right`() {
-    val actual: Either<Nothing, String> = "something".right()
-    assertThat(actual).containsRightInstanceOf(String::class.java)
-  }
+    @Test
+    fun `should pass if either contains required type on right`() {
+        val actual: Either<Nothing, String> = "something".right()
+        assertThat(actual).containsRightInstanceOf(String::class.java)
+    }
 }
 
 private open class Parent
