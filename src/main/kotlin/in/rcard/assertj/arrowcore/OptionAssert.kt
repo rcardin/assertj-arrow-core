b0590dc6ea0c82165e638bd85aa3c1e2532b5e4e
rcardin <riccardo.cardin@gmail.com>
2023-03-17 16:38:47 +0100

    Added some documentation

--- src/main/kotlin/in/rcard/assertj/arrowcore/OptionAssert.kt
+++ src/main/kotlin/in/rcard/assertj/arrowcore/OptionAssert.kt
@@ -2,6 +2,12 @@ package `in`.rcard.assertj.arrowcore
 
 import arrow.core.Option
 
+/**
+ * Assertions for [Option].
+ *
+ * @param VALUE type of the value contained in the [Option].
+ * @author Riccardo Cardin
+ */
 class OptionAssert<VALUE : Any>(option: Option<VALUE>?) :
     AbstractOptionAssert<OptionAssert<VALUE>, VALUE>(option) {
     companion object {
