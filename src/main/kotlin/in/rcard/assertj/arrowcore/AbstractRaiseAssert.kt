152ea0902cacc584e59329f861e3acd07ebd4a15
rcardin <riccardo.cardin@gmail.com>
2023-10-17 21:59:03 +0200

    [raise-dsl-assertions] WIP Trying to understand how to pass lambda information

--- src/main/kotlin/in/rcard/assertj/arrowcore/AbstractRaiseAssert.kt
+++ src/main/kotlin/in/rcard/assertj/arrowcore/AbstractRaiseAssert.kt
@@ -1,9 +1,27 @@
 package `in`.rcard.assertj.arrowcore
 
 import arrow.core.raise.Raise
+import arrow.core.raise.fold
 import org.assertj.core.api.AbstractAssert
+import org.assertj.core.api.Assertions
 
 abstract class AbstractRaiseAssert<
     SELF : AbstractRaiseAssert<SELF, ERROR, VALUE>, ERROR, VALUE : Any,
     >(lambda: context(Raise<ERROR>) () -> VALUE) :
-    AbstractAssert<SELF, context(Raise<ERROR>) () -> VALUE>(lambda, AbstractRaiseAssert::class.java)
+    AbstractAssert<SELF, context(Raise<ERROR>) () -> VALUE>(lambda, AbstractRaiseAssert::class.java) {
+
+    fun succeedsWith(value: VALUE): SELF {
+        fold(
+            block = actual,
+            recover = { error: ERROR -> failWithMessage("Expected lambda to succeed but it failed with $error") },
+            transform = { Assertions.assertThat(it).isEqualTo(value) },
+            catch = { ex: Throwable ->
+                when (ex) {
+                    is AssertionError -> throw ex
+                    else -> failWithMessage("Expected lambda to succeed but it throws the exception $ex")
+                }
+            },
+        )
+        return myself
+    }
+}
